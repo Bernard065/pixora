2837f5d589b5cc1f9d66464e854982f999351196
Bernard <bebenibernard@gmail.com>
2026-02-12 02:29:37 +0300

    Add features section showcasing Pixora AI capabilities

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Features from '@/modules/features'
 import Hero from '@/modules/hero'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const Home = () => {
   return (
     <div>
       <Hero />
+      <Features />
     </div>
   )
 }
