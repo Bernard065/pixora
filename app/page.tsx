b0a8974db8401ac47f5c69836c6a08e046af2cdf
Bernard <bebenibernard@gmail.com>
2026-02-20 01:12:40 +0300

    Implement footer UI and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/footer'
 import Editor from '@/modules/editor'
 import Features from '@/modules/features'
 import Hero from '@/modules/hero'
@@ -10,6 +11,7 @@ const Home = () => {
       <Features />
       <Pricing />
       <Editor />  
+      <Footer />
     </div>
   )
 }
